f81fb30a0b45a22aff9159d4f58cfc2916ac091f
Tsang Jun Wen <junwen2992@gmail.com>
2022-02-04 12:24:15 +0800

    fixed wrong click event position

--- src/components/DropDownMenu.vue
+++ src/components/DropDownMenu.vue
@@ -1,18 +1,14 @@
 <template>
   <v-menu>
     <template v-slot:activator="{ attrs, on }">
-      <v-app-bar-nav-icon v-bind="attrs" v-on="on" />
+      <v-app-bar-nav-icon v-bind="attrs" v-on="on"> </v-app-bar-nav-icon>
     </template>
-
     <v-list>
-      <v-list-item link>
+      <v-list-item link @click="logout">
         <v-list-item-icon>
           <v-icon v-text="'mdi-logout'"></v-icon>
         </v-list-item-icon>
-        <v-list-item-title
-          v-text="'Logout'"
-          @click.prevent="logout"
-        ></v-list-item-title>
+        <v-list-item-title v-text="'Logout'"></v-list-item-title>
       </v-list-item>
     </v-list>
   </v-menu>
