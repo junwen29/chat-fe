1169be8892f160a35f0ef746694b981ea196a552
Tsang Jun Wen <junwen2992@gmail.com>
2022-02-20 10:39:50 +0800

    removed message groups mocking

--- src/components/MessageGroupList.vue
+++ src/components/MessageGroupList.vue
@@ -1,6 +1,8 @@
 <template>
   <v-list id="MessageGroupList">
-    <template v-for="(messageGroup, index) in selectedChatRoomMessages.messageGroups">
+    <template
+      v-for="(messageGroup, index) in selectedChatRoomMessages.messageGroups"
+    >
       <!-- for each message group  -->
       <v-list-item :key="index">
         <v-list-item-content>
@@ -18,19 +20,12 @@
 
 <script>
 import MessageList from "./MessageList.vue";
-import { createMessageGroup } from "../_mock";
 import { mapState } from "vuex";
 
 export default {
   components: {
     MessageList,
   },
-  data: () => ({
-    messageGroups: [
-      createMessageGroup("Yesterday", 6),
-      createMessageGroup("Today", 3),
-    ],
-  }),
   computed: {
     ...mapState("chatMessages", ["selectedChatRoomMessages"]),
   },
